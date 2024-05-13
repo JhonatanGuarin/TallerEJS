5b5b131337d6954da46066365c216f5e965d18cb
JhonatanGuarin <jhonatancamilo99@gmail.com>
2024-05-13 03:08:45 -0500

    Fromulario Terminado

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require("express");//express es el framework web principal que se utilizará para crear la aplicación
 const path = require("path");//path es un módulo incorporado en Node.js para trabajar con rutas de archivos.
-
 const app = express();// crea una instancia dela aplicacion express
 
-//setters
 
-app.use(express.json());//permite analizar los cuerpos de las solicitudes entrantes en formato JSON.
-app.set("views", "./views");//configuran el directorio de vistas (./views) 
-
-app.set(" engine", "ejs");
 app.set("PORT", process.env.PORT || 3000);
-app.use(express.static("public"));//Estas líneas configuran los "middleware" de Express sirve archivos estáticos (como CSS, JavaScript e imágenes) desde el directorio public
 
-// Servir archivos estáticos desde la carpeta 'public/css'
-app.use('/css', express.static(path.join(__dirname, 'public/css')));
-
-app.use('/css', express.static(path.join(__dirname, 'public/img')));
-//midleware
-app.use("/", require("./routes/index"));
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(app.get("PORT"), () =>
     console.log(`Server listen at Port ${app.get("PORT")}`)
 );
+
+// Settings
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+// Middlewares
+app.use(express.urlencoded({extended: false}))
+
+
+// Routes
+app.use(require('./routes/index'))
+
+
+//app.use(express.json());//permite analizar los cuerpos de las solicitudes entrantes en formato JSON.
+
+// Static
+app.use(express.static("public"));//Estas líneas configuran los "middleware" de Express sirve archivos estáticos (como CSS, JavaScript e imágenes) desde el directorio public
+// Servir archivos estáticos desde la carpeta 'public/css'
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/css', express.static(path.join(__dirname, 'public/img')));
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,35 +6,22 @@ const {v4: uuidv4} = require('uuid')
 
 // Leer el archivo shoes.json
 const shoeDataPath = path.join(__dirname, "..", "resource", "file", "shoes.json");
-let shoesData = JSON.parse(fs.readFileSync(shoeDataPath, "utf-8"));
+let Shoes = JSON.parse(fs.readFileSync(shoeDataPath, "utf-8"));
 
 router.get("/", (req, res) => {
-  res.render("index.ejs", { title: "Gestión de zapatos", data: shoesData });
-  console.log(shoesData);
+  res.render("index.ejs", { title: "Gestión de zapatos", data: Shoes });
+  console.log(Shoes);
 });
 
 router.get("/inventory", (req, res) => {
-  if (shoesData.length > 0) {
-    res.render("inventory.ejs", { title: "Gestión de zapatos", data: shoesData });
-    console.log(shoesData);
+  if (Shoes.length > 0) {
+    res.render("inventory.ejs", { title: "Gestión de zapatos", data: Shoes });
+    console.log(Shoes);
   } else {
     console.log("no hay datos");
      }
 });
 
-router.get("/get-shoes/:marca", (req, res) => {
-  const { marca } = req.params;
-  const shoe = shoesData.find((shoes) => shoes.modelo === marca);
-
-  if (shoe) {
-    return res.status(200).json({ state: true, data: shoe });
-  }
-  return res.status(404).json({ state: false, message: "Zapato no encontrado" });
-});
-
-router.get("/inventarios", (req, res) => {
-  res.render("inventario.ejs", { title: "direccionar pg" });
-});
 
 router.get("/addproducts", (req, res) => {
   res.render("addproducts.ejs");
@@ -43,38 +30,39 @@ router.get("/addproducts", (req, res) => {
 
 router.post("/addproducts", (req, res) => {
     
-console.log(req.body.Model)
-
-  const {Brand, Model, Color, Size, Price, Image} = req.body
-
-  if(!Brand || !Model || !Color || !Size || !Price || !Image) {
-      res.status(400).send("Campos incompletos")
-      return
-  }
-
-  let newShoe = {
+  console.log(req.body.Model)
+  
+    const {Brand, Model, Color, Size, Price, Image, Stock} = req.body
+  
+    if(!Brand || !Model || !Color || !Size || !Price || !Image || !Stock) {
+        res.status(400).send("Campos incompletos")
+        return
+    }
+  
+    let newShoe = {
       id: uuidv4(),
       Brand,
       Model,
       Color,
       Size,
       Price,
-      Image
-  }
-
-  shoesData.push(newShoe)
-
-
-  const shoeDataPath = path.join(__dirname, "..", "resource", "file", "shoes.json");
-
-
-  const  json_shoes = JSON.stringify(shoesData)
-  fs.writeFileSync(shoeDataPath, json_shoes, 'utf-8')
-
-
-  console.log(req.body)
-  res.redirect('/')
-})
+      Image,
+      Stock
+    }
+  
+    Shoes.push(newShoe)
+  
+  
+    const shoeDataPath = path.join(__dirname, "..", "resource", "file", "shoes.json");
+  
+  
+    const  json_shoes = JSON.stringify(Shoes)
+    fs.writeFileSync(shoeDataPath, json_shoes, 'utf-8')
+  
+  
+    console.log(req.body)
+    res.redirect('/')
+  })
 
 
 module.exports = router;
